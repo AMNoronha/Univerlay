25cffd491d42599c771b117a588a92ba6d9e26d8
Alexander Noronha <alexander@noronha.asia>
2021-12-15 20:47:42 +0800

    controllers generated

---
+++ app/controllers/lesson_steps_controller.rb
@@ -0,0 +1,2 @@
+class LessonStepsController < ApplicationController
+end
---
+++ test/controllers/lesson_steps_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LessonStepsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
