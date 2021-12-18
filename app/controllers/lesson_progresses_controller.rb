7873bef87f9a7eccf04f4c52d12435b0167fe43d
Timothy Yeh <jcluvt@yahoo.com>
2021-12-18 14:53:53 +0800

    partial work on controllers

--- app/controllers/lesson_progresses_controller.rb
+++ app/controllers/lesson_progresses_controller.rb
@@ -1,2 +1,51 @@
 class LessonProgressesController < ApplicationController
+  before_action :set_lesson_progress, only: %i[show edit update]
+
+  def index
+    @lesson_progresses = policy_scope(Lesson_progress)
+  end
+
+  def show
+    @lessons = Lesson.all
+    @lesson = @lessons.find_by(lesson_id: @lesson_progress.id)
+  end
+
+  def new
+    @lesson_progress = LessonProgress.new
+    authorize @lesson_progress
+  end
+
+  def create
+    @lesson_progress = LessonProgress.new(lesson_progress_params)
+    @lesson_progress.user = current_user
+    authorize @lesson_progress
+
+    if @lesson_progress.save
+      redirect_to @lesson_progress
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @lesson_progress.update(lesson_progress_params)
+      redirect_to @lesson_progress
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_lesson_progress
+    @lesson_progress = LessonProgress.find(params[:id])
+    authorize @lesson_progress
+  end
+
+  def lesson_progress_params
+    params.require(:lesson_progress).permit(:current_step, photos: [])
+  end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,9 +1,10 @@
 class Lesson < ApplicationRecord
+  include PgSearch::Model
   pg_search_scope :search_by_title,
-  against: [ :title ],
-  using: {
-    tsearch: { prefix: true }
-  }
+    against: [ :title ],
+    using: {
+      tsearch: { prefix: true }
+    }
 
   has_many :lesson_progresses, dependent: :destroy
   has_many :lesson_steps, dependent: :destroy
