1c0c45370a81c79f11b98d6c0910d5f3697a83a2
Timothy Yeh <jcluvt@yahoo.com>
2021-12-20 19:51:30 +0800

    remove page policy fix lesson policy

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,6 @@
 class LessonsController < ApplicationController
-  # def index
-  #   @lessons = policy_scope(Lesson)
-  # end
-
   def show
     @lesson = Lesson.find(params[:id])
+    authorize @lesson
   end
 end
