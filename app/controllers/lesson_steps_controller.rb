d8ac1f774c7c3ea2fe5b98284e0986618e8d7d8e
Alexander Noronha <alexander@noronha.asia>
2022-01-07 18:04:06 +0800

    added changes to seed file (URL converted to REGEX)

--- app/controllers/lesson_steps_controller.rb
+++ app/controllers/lesson_steps_controller.rb
@@ -2,7 +2,7 @@ class LessonStepsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    lesson = policy_scope(Lesson).find(1)
+    lesson = policy_scope(Lesson).find(params[:lesson_id])
     @lesson_steps = lesson.lesson_steps
     respond_to do |format|
       format.json { render json: @lesson_steps }
--- db/seeds.rb
+++ db/seeds.rb
@@ -63,7 +63,7 @@ LessonStep.create!(
   DOM_Id: ".octicon.octicon-plus",
   pop_up_text: "Select 'New repository'",
   sequence: 1,
-  url: "https://github.com/"
+  url: "^https://github.com/$"
 )
 
 LessonStep.create!(
@@ -72,7 +72,7 @@ LessonStep.create!(
   DOM_Id: ".js-template-repository-select",
   pop_up_text: "Select a template, if you would like to (not necessary)",
   sequence: 2,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -81,7 +81,7 @@ LessonStep.create!(
   DOM_Id: "#repository_name",
   pop_up_text: "Enter short memorable name for the repository",
   sequence: 3,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -90,7 +90,7 @@ LessonStep.create!(
   DOM_Id: "#repository_description",
   pop_up_text: "Enter a short description if you'd like to do so",
   sequence: 4,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -99,7 +99,7 @@ LessonStep.create!(
   DOM_Id: ".js-privacy-toggle-label-public",
   pop_up_text: "Select if your repository should be public or private",
   sequence: 5,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -108,7 +108,7 @@ LessonStep.create!(
   DOM_Id: "#repository_auto_init",
   pop_up_text: "We suggest clicking this to add a README file",
   sequence: 6,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -117,7 +117,7 @@ LessonStep.create!(
   DOM_Id: "#repository_gitignore_template_toggle",
   pop_up_text: "We also suggest adding a .gitignore file",
   sequence: 7,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -126,7 +126,7 @@ LessonStep.create!(
   DOM_Id: "#repository_license_template_toggle",
   pop_up_text: "Add a license if desired",
   sequence: 8,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 LessonStep.create!(
@@ -135,7 +135,71 @@ LessonStep.create!(
   DOM_Id: ".btn-primary",
   pop_up_text: "Press to create the new respository",
   sequence: 9,
-  url: "https://github.com/new"
+  url: "^https://github.com/new$"
 )
 
 puts "Lesson steps for Create Github Repo created!"
+
+LessonProgress.create!(
+  user_id: 1,
+  lesson_id: 3,
+  current_step: 1
+)
+LessonStep.create!(
+  lesson_id: 3,
+  title: "Step 1",
+  DOM_Id: "#pull-requests-tab",
+  pop_up_text: "At your local repository (on your terminal), make sure you have run git add, git commit & git push origin branch-name",
+  sequence: 1,
+  url: "^https://github.com/\\w+/\\w+-?\\w*-?\\w*-?\\w*$"
+)
+LessonStep.create!(
+  lesson_id: 3,
+  title: "Step 2",
+  DOM_Id: ".flex-shrink-0.btn.btn-primary.ml-sm-3.mt-2.mt-sm-n2.mb-sm-n2.mr-sm-n1.flex-self-center",
+  pop_up_text: "Click the compare & pull request button",
+  sequence: 2,
+  url: "^https://github.com/.+/.+/pulls$"
+)
+# LessonStep.create!(
+#   lesson_id: 3,
+#   title: "Step 3",
+#   DOM_Id: "#pull_request_title",
+#   pop_up_text: "Enter short memorable title for this pull request",
+#   sequence: 3,
+#   url: "https://github.com/"
+# )
+# LessonStep.create!(
+#   lesson_id: 3,
+#   title: "Step 4",
+#   DOM_Id: "#pull_request_body",
+#   pop_up_text: "Write comments on what changes you have made to the code",
+#   sequence: 4,
+#   url: "https://github.com/"
+# )
+# LessonStep.create!(
+#   lesson_id: 3,
+#   title: "Step 5",
+#   DOM_Id: ".BtnGroup",
+#   pop_up_text: "Click the Create Pull Request button",
+#   sequence: 5,
+#   url: "https://github.com/"
+# )
+# LessonStep.create!(
+#   lesson_id: 3,
+#   title: "Step 6",
+#   DOM_Id: ".btn-group-merge",
+#   pop_up_text: "Click the merge button if there are no conflicts with the base branch",
+#   sequence: 6,
+#   url: "https://github.com/"
+# )
+# LessonStep.create!(
+#   lesson_id: 3,
+#   title: "Step 7",
+#   DOM_Id: "#.btn-primary",
+#   pop_up_text: "Click the confirm merge button",
+#   sequence: 7,
+#   url: "https://github.com/"
+# )
+
+puts "Lesson steps for Create Github Pull request created!"
