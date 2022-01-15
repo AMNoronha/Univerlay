2f883246317847e37749696932ed9eeab837a364
Alexander Noronha <alexander@noronha.asia>
2022-01-15 17:33:25 +0800

    this works with dashboard open in another tab

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -1,5 +1,5 @@
 class Api::LessonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  # skip_before_action :authenticate_user!, only: [:index]
   # skip_before_action :verify_authenticity_token
   def index
     @lessons = policy_scope(Lesson, policy_scope_class: Api::LessonPolicy::Scope)
